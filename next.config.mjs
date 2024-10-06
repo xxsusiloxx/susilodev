d68489ccdeb5396dbb1071b2ac17582e212a8fd3
Susilloxxxx <susiloabdulmujib@outlook.com>
2024-10-06 22:05:31 +0700

    chore: eslint && prettier

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["https://lh3.googleusercontent.com"],
+    domains: ['https://lh3.googleusercontent.com'],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-import dayjs from "dayjs";
-import relativeTimePlugin from "dayjs/plugin/relativeTime";
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import dayjs from 'dayjs';
+import relativeTimePlugin from 'dayjs/plugin/relativeTime';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -13,11 +13,9 @@ export function relativeTime(dateAsString: string) {
 }
 
 export function bytesToSize(bytes: number): string {
-  const sizes: string[] = ["Bytes", "KB", "MB", "GB", "TB"];
-  if (bytes === 0) return "n/a";
-  const i: number = parseInt(
-    Math.floor(Math.log(bytes) / Math.log(1024)).toString()
-  );
+  const sizes: string[] = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+  if (bytes === 0) return 'n/a';
+  const i: number = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)).toString());
   if (i === 0) return `${bytes} ${sizes[i]}`;
   return `${(bytes / Math.pow(1024, i)).toFixed(1)} ${sizes[i]}`;
 }
