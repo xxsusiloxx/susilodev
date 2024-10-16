8270c856a2e52835168d233572376a5584eaff6f
Susilloxxxx <susiloabdulmujib@outlook.com>
2024-10-16 17:39:43 +0700

    feat(s): s

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,8 +1,11 @@
 const path = require('path');
 
+// Fungsi untuk menjalankan ESLint dengan opsi --fix
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
 
+// Export konfigurasi lint-staged
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand, 'prettier --write'], // Jalankan ESLint dan Prettier pada file JavaScript/TypeScript
+  '*.{json,css,md}': ['prettier --write'], // Jalankan Prettier pada file JSON, CSS, dan Markdown
 };
