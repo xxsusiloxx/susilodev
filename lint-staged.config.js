9761970fd2703c57fbc1dae1c8630e8caf50cff7
Susilloxxxx <susiloabdulmujib@outlook.com>
2024-10-16 22:40:53 +0700

    ci(husky): eslint
    adding eslint, prettier and lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,9 @@
 const path = require('path');
 
-// Fungsi untuk menjalankan ESLint dengan opsi --fix
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
 
 module.exports = {
   '*.{js,jsx,ts,tsx}': [buildEslintCommand, 'prettier --write'], // Jalankan ESLint dan Prettier di file JS/TS
-  '*.{json,css,md}': ['prettier --write'], // Jalankan Prettier di file JSON, CSS, Markdown
+  '*.{json,css,md}': ['prettier --write'], // Jalankan Prettier di file JSON, CSS, dan Markdown
 };
