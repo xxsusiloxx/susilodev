a9cd341419a48ece37df7c302fafda50df57cca7
Susilloxxxx <susiloabdulmujib@outlook.com>
2024-10-21 10:58:12 +0700

    feat(refactor): e2e
    remove unnecessary file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import ImageUpload from '@/components/modules/ImageUpload';
-
 export default function Home() {
-  return (
-    <div className="mx-auto flex w-full max-w-[1350px] flex-col">
-      <ImageUpload />
-    </div>
-  );
+  return <div className="mx-auto flex w-full max-w-[1350px] flex-col"></div>;
 }
